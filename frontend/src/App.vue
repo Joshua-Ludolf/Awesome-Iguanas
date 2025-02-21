48af020ed5810d2ebbcefe7897e6a0eb19f8eabb
Yesmin301 <yesminhernandezreyna@gmail.com>
2025-02-20 20:49:06 -0600

    2/20/25
    A simple visual of the containers.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,7 +12,19 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+
+  <div class="container">
+      <!--This is for text box #1-->
+        <input text="text" placeholder= "Enter code: ">
+      <!--Box numero 2-->
+        <input text="text" placeholder= "output ig: ">
+    </div>
+
+    <div class="d-grid">
+      <button type="submit" class="submit-btn">Submit</button>
+    </div>
 </template>
+
 
 <style scoped>
 .logo {
@@ -27,4 +39,28 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+.container{
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: auto;
+  gap: 40px;
+  
+}
+/*PLEASE BE THE PERFECT SIZE BESTIE(idk if i like having style and html here.........sus... >-<)*/
+input{
+  width: 300px;
+  height: 300px;
+  padding: auto;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 3px solid;
+}
+.submit-btn{
+  margin-top: 10px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+
 </style>
